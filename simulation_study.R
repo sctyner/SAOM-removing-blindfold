ac2c09244d46e58efc514b9d2d7b9dc4b4f9759c
Samantha Tyner <sctyner@iastate.edu>
2016-06-12 19:02:17 -0400

    start

---
+++ simulation_study.R
@@ -0,0 +1,7 @@
+# simulations
+
+# plan: 
+# 1. simulate 1000 networks from the mean values previously used as starting values for each model
+# 2. combine the 1000 networks into one data set and visualize them ontop of each other.
+# 3. repeat for models M1, M2, M3
+
